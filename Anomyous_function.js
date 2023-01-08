94782f5e9d9ab531264679043c410561ba273219
Ashutosh Pandey <121302750+ashutosh2773@users.noreply.github.com>
2023-01-08 10:06:31 -0500

    Add files via upload

---
+++ Anomyous_function.js
@@ -0,0 +1,15 @@
+function itm(a,b){
+//     // console.log("hello itm");
+//     // console.lof(a+b)
+//     return a+b;
+// }
+// const d = itm(4,5)
+// console.log(d)
+}//
+
+
+const user = function(a,b){
+    console.log('hloo ashutosh');
+    console.log(a+b);
+}
+user(5,4);
